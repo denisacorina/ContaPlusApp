03be413fced0e18a7d942b8160b632dced8d84ec
Deni <denisa.obreja@yahoo.com>
2022-11-30 01:07:28 +0200

    LoginForm

--- ContaPlusUI/src/app/app.module.ts
+++ ContaPlusUI/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MaterialModule } from './shared/modules/material/material.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
+
+    MatFormFieldModule,
+    MatInputModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
